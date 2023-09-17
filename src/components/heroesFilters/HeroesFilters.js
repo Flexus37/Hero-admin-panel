a18ed5b2f526a11db2f78e6a724cecc2e7d6106b
Flexus37 <aleshadudin53@gmail.com>
2023-09-17 16:29:05 +0300

    React+Redux before usin RTK Query

---
+++ src/components/heroesFilters/HeroesFilters.js
@@ -0,0 +1,70 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import classNames from "classnames";
+
+import store from "../../store";
+import { activeFilterChanged, fetchFilters, selectAll } from "./filtersSlice";
+import Spinner from "../spinner/Spinner";
+
+// Задача для этого компонента:
+// Фильтры должны формироваться на основании загруженных данных
+// Фильтры должны отображать только нужных героев при выборе
+// Активный фильтр имеет класс active
+// Изменять json-файл для удобства МОЖНО!
+// Представьте, что вы попросили бэкенд-разработчика об этом
+
+const HeroesFilters = () => {
+
+    const {filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
+    const filters = selectAll(store.getState());
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchFilters());
+        // eslint-disable-next-line
+    }, []);
+
+    if (filtersLoadingStatus === 'loading') {
+        return <Spinner/>
+    } else if (filtersLoadingStatus === 'error') {
+        return <h5>Произошла ошибка...</h5>
+    }
+
+    const renderFilters = (arr) => {
+        if (arr.length === 0) {
+            <h5>Фильтров пока нет...</h5>
+        }
+
+        const items = arr.map(({name, label, className}) => {
+            return (
+                <button
+                key={name}
+                id={name}
+                className={classNames('btn', className, {
+                    'active': name === activeFilter
+                })}
+                onClick={() => dispatch(activeFilterChanged(name))}
+                >
+                {label}
+                </button>
+            );
+        });
+
+        return items;
+    }
+
+    const elements = renderFilters(filters);
+
+    return (
+        <div className="card shadow-lg mt-4">
+            <div className="card-body">
+                <p className="card-text">Отфильтруйте героев по элементам</p>
+                <div className="btn-group">
+                    {elements}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroesFilters;
---
+++ src/components/heroesList/HeroesList.js
@@ -0,0 +1,69 @@
+import {useHttp} from '../../hooks/http.hook';
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+
+import { fetchHeroes, heroDeleted, filteredHeroesSelector } from './heroesSlice';
+
+import HeroesListItem from "../heroesListItem/HeroesListItem";
+import Spinner from '../spinner/Spinner';
+
+// Задача для этого компонента:
+// При клике на "крестик" идет удаление персонажа из общего состояния
+// Усложненная задача:
+// Удаление идет и с json файла при помощи метода DELETE
+
+const HeroesList = () => {
+
+    // Неоптимизированный вариант создания селектора
+    // const filteredHeroes = useSelector(state => {
+    //     if (state.filters.activeFilter === 'all') {
+    //         return state.heroes.heroes;
+    //     } else {
+    //         return state.heroes.heroes.filter(item => item.element === state.activeFilter)
+    //     }
+    // })
+
+    const filteredHeroes = useSelector(filteredHeroesSelector);
+    const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
+    const dispatch = useDispatch();
+    const {request} = useHttp();
+
+    useEffect(() => {
+        dispatch(fetchHeroes());
+        // eslint-disable-next-line
+    }, []);
+
+    const onDelete = useCallback((id) => {
+        request(`http://localhost:3001/heroes/${id}`, 'DELETE')
+            // .then(data => console.log(data, 'Delete!'))
+            .then(dispatch(heroDeleted(id)))
+            .catch((err) => console.log(err));
+    }, [request]);
+
+    if (heroesLoadingStatus === "loading") {
+        return <Spinner/>;
+    } else if (heroesLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
+
+    const renderHeroesList = (arr) => {
+        if (arr.length === 0) {
+            return <h5 className="text-center mt-5">Героев пока нет</h5>
+        }
+
+        return arr.map(({id, ...props}) => {
+            return <HeroesListItem key={uuidv4()} onDelete={() => onDelete(id)} {...props}/>
+        })
+    }
+
+    const elements = renderHeroesList(filteredHeroes);
+
+    return (
+        <ul>
+            {elements}
+        </ul>
+    )
+}
+
+export default HeroesList;
